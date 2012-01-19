a15616850f0d12e401a3b90bce2cf362d0a2b0b5
Divendo1180@gmail.com <Divendo1180@gmail.com@b84ececf-d5dc-c573-e7c7-584b25a6d52f>
2012-01-19 22:43:43 +0000

    Improving the program structure some more.

--- trunk/mainwidget.cpp
+++ trunk/mainwidget.cpp
@@ -1,40 +1,60 @@
 #include "mainwidget.h"
+#include "xordialog.h"
 
-MainWidget::MainWidget(QWidget *parent) : QWidget(parent)
-{
-    setStyleSheet("QPushButton {font:bold}");
-    encryptionBox = new QComboBox(this);
-    encryptionBox->addItem("XOR");
-    encryptionBox->addItem("Caesar");
-    encryptionBox->addItem("TEA");
-    encryptionBox->setFixedWidth(75);
-    startButton = new QPushButton("Start!", this);
-    startButton->setFixedSize(150, 30);
-    encryptorLabel = new QLabel("Encryptor r3");
-    encryptorLabel->setStyleSheet("font:bold 30px");
-
-    QVBoxLayout* mainLayout = new QVBoxLayout();
-    QHBoxLayout* encryptLayout= new QHBoxLayout;
-    encryptLayout->addWidget(encryptionBox);
-    encryptLayout->addWidget(startButton);
-    mainLayout->addWidget(encryptorLabel);
-    mainLayout->addLayout(encryptLayout);
-
-    setLayout(mainLayout);
-
-    connect(startButton, SIGNAL(clicked()), this, SLOT(start()));
-}
-
-MainWidget::~MainWidget()
-{
-
-}
-
-void MainWidget::start()
-{
-    if(encryptionBox->currentText() == "XOR")
+// Public:
+    MainWidget::MainWidget(QWidget* parent)
+    : QMainWindow(parent), dlgEncrypt(0)
     {
-        XORDialog* dlg = new XORDialog;
-        dlg->show();
+        // Some general settings for this window
+        setWindowTitle("Encryptor");
+        setStyleSheet("QPushButton {font:bold}");
+
+        // Create the interface
+        encryptionBox = new QComboBox(this);
+        encryptionBox->addItem("XOR");
+        encryptionBox->addItem("Caesar");
+        encryptionBox->addItem("TEA");
+        startButton = new QPushButton("Start!", this);
+        encryptorLabel = new QLabel("Encryptor r3");
+        encryptorLabel->setStyleSheet("font:bold 30px");
+
+        // Create the central widget
+        QWidget* centWidget = new QWidget(this);
+        QVBoxLayout* mainLayout = new QVBoxLayout();
+        QHBoxLayout* encryptLayout= new QHBoxLayout();
+        mainLayout->addWidget(encryptorLabel);
+        encryptLayout->addWidget(encryptionBox);
+        encryptLayout->addWidget(startButton);
+        mainLayout->addLayout(encryptLayout);
+        centWidget->setLayout(mainLayout);
+        setCentralWidget(centWidget);
+
+        // Connect the interface to the necessary slots
+        connect(startButton, SIGNAL(clicked()), this, SLOT(start()));
     }
-}
+
+    MainWidget::~MainWidget()
+    {
+        delete encryptionBox;
+        delete startButton;
+        delete encryptorLabel;
+        if(dlgEncrypt)  delete dlgEncrypt;
+    }
+
+// Public slots:
+    void MainWidget::start()
+    {
+        // Clean up the old dialog
+        if(dlgEncrypt)
+        {
+            delete dlgEncrypt;
+            dlgEncrypt = 0;
+        }
+
+        // Determine what type of dialog should be created
+        if(encryptionBox->currentText() == "XOR")
+        {
+            dlgEncrypt = new XORDialog(this);
+            dlgEncrypt->show();
+        }
+    }
